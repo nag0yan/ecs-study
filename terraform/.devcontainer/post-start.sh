fcd6439a454b4eab621480eda4aa16e7214fea6c
nag0yan <matsui-ka@kadokawa.jp>
2023-09-28 04:37:02 +0000

    setup

--- terraform/.devcontainer/post-start.sh
+++ terraform/.devcontainer/post-start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# For aws configure
+sudo chown vscode /home/vscode/.aws
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
